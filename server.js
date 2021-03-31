f1664a690a11f79fc567fc1e8d0c7b6ed4c5ff94
Ashraf Talukder <ashraf.talukder996@hotmail.com>
2021-03-30 23:17:20 -0500

    latest

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const server = express()
+server.use(bodyParser.json())
+const port = process.argv[2] || 4000
+
+const options = [
+    "option-1",
+    "option-2",
+    "option-3",
+    "option-4",
+    "option-5",
+]
+
+const radomIndex = Math.floor(Math.random() * options.length)
+server.get('/api', (req, res)=> {
+     console.log(" === requesting =====")
+    res.json({
+         data: options[radomIndex],
+        port:port,
+        process: process.pid,
+      
+    })
+})
+
+
+
+
+
+server.listen(port, ()=>{
+    console.log(`== api is working on port ${port} ==`)
+})
